6cacc9756087be11834e63dd2df38852f76fbf21
HoangBui107 <hoangbui100701@gmail.com>
2024-11-27 09:15:09 +0700

    feat[hoang-23]: Product Admin

--- src/routes/admin/product.js
+++ src/routes/admin/product.js
@@ -3,13 +3,43 @@ import {
   requirePermission,
   verifyAccessToken,
 } from '../../middlewares/authMiddleware.js'
-import { adminCreateProduct } from '../../controllers/admin/productController.js'
+import {
+  adminCreateProduct,
+  adminDeleteProduct,
+  adminEditProduct,
+  adminGetListProduct,
+  adminGetProductDetail,
+} from '../../controllers/admin/productController.js'
 const Router = express.Router()
+
+Router.route('/list').get(
+  verifyAccessToken,
+  requirePermission('create_product'),
+  adminGetListProduct
+)
+
+Router.route('/detail/:id').get(
+  verifyAccessToken,
+  requirePermission('create_product'),
+  adminGetProductDetail
+)
 
 Router.route('/create').post(
   verifyAccessToken,
   requirePermission('create_product'),
   adminCreateProduct
+)
+
+Router.route('/delete/:id').delete(
+  verifyAccessToken,
+  requirePermission('create_product'),
+  adminDeleteProduct
+)
+
+Router.route('/edit/:id').put(
+  verifyAccessToken,
+  requirePermission('create_product'),
+  adminEditProduct
 )
 
 export const productAdminRoute = Router
