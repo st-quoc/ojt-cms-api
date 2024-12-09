e01a2e4ec53692b7365610b4e0edc401d16dc985
Doan Quoc Hoa <109152800+quochoa304@users.noreply.github.com>
2024-12-09 20:19:55 +0700

    Feat[hoa-29]: Payment process

--- src/controllers/user/cartController.js
+++ src/controllers/user/cartController.js
@@ -185,3 +185,27 @@ export const updateQuantity = async (req, res) => {
       .json({ message: 'Failed to update cart', error: error.message })
   }
 }
+export const deleteAllProductInCart = async (req, res) => {
+  try {
+    const { userId } = req.body
+
+    if (!userId) {
+      return res.status(400).json({ message: 'User ID is required' })
+    }
+
+    const cart = await Cart.findOne({ userId })
+
+    if (!cart) {
+      return res.status(404).json({ message: 'Cart not found' })
+    }
+
+    cart.items = []
+    await cart.save()
+
+    res.status(200).json({ message: 'All products removed from cart', cart })
+  } catch (error) {
+    res
+      .status(500)
+      .json({ message: 'Failed to clear cart', error: error.message })
+  }
+}
--- src/routes/user/cartRoute.js
+++ src/routes/user/cartRoute.js
@@ -4,6 +4,7 @@ import {
   getCartItems,
   removeItemFromCart,
   updateQuantity,
+  deleteAllProductInCart,
 } from '../../controllers/user/cartController.js'
 
 const Router = express.Router()
@@ -12,4 +13,5 @@ Router.route('/list/:userId').get(getCartItems)
 Router.route('/add').post(addToCart)
 Router.route('/update').put(updateQuantity)
 Router.route('/remove').delete(removeItemFromCart)
+Router.route('/delete-all').delete(deleteAllProductInCart)
 export const cartRoutes = Router
